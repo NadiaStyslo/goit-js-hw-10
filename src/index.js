512de638fa0ed836ac2dd87065fab8d029a94b34
NadiaStyslo <sir.ihor@gmail.com>
2023-12-08 21:42:26 -0500

    started hw

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+const selectCat = document.querySelector('.breed-select');
+const loadCat = document.querySelector('.loader');
+const infoCat = document.querySelector('.cat-info');
+
